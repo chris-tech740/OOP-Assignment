31571752a94e7613f0f29329da5bfa810312252f
27545 Chris IMANZI <chrisimanzi30@gmail.com>
2025-11-23 00:48:01 +0200

    Add files via upload

---
+++ IMANZI Chris 2/q10/DataException.java
@@ -0,0 +1,7 @@
+package c27545.q10;
+
+public class DataException extends Exception {
+    public DataException(String message) {
+        super(message);
+    }
+}
---
+++ IMANZI Chris 2/q9/DataException.java
@@ -0,0 +1,5 @@
+package c27545.q9;
+
+public class DataException extends Exception {
+    public DataException(String message) { super(message); }
+}
